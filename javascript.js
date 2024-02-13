3913505d4594bb70486d09980734ae62ae265932
joshuazhou08 <joshuazhou0808@gmail.com>
2024-02-13 14:54:17 -0800

    Add Fadein Scroll Animations

--- javascript.js
+++ javascript.js
@@ -3,3 +3,19 @@ const newsletter_link = document.querySelector('#newsletter-link')
 const newsletter = document.querySelector(".newsletter")
 
 newsletter_link.addEventListener("click", ()=>{newsletter.scrollIntoView({behavior: "smooth"})})
+
+//Fade scroll animations using Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('fadeup')
+        } else {
+            entry.target.classList.remove('fadeup')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+})
