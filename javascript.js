3b4a8b2331a11d6e1bce2abfa6e7d0ca9a07e2a5
joshuazhou08 <joshuazhou0808@gmail.com>
2024-02-25 21:49:16 -0800

    Add Realtime Database With Google Firebase

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 const newsletter_link = document.querySelector('#newsletter-link')
 
 const newsletter = document.querySelector(".newsletter")
@@ -19,3 +21,5 @@ const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((element) => {
     observer.observe(element);
 })
+
+
